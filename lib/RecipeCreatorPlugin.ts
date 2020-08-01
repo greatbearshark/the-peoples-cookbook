b446221a0d4fbb5202bad533d516984cd02bbe1e
greatbearshark <greatbearshark@protonmail.com>
2020-08-01 18:56:06 -0500

    Fix recipe creator plugin

--- lib/RecipeCreatorPlugin.ts
+++ lib/RecipeCreatorPlugin.ts
@@ -1,3 +1,24 @@
+import { CMS } from 'tinacms';
+
+function slugify(string: string): string {
+  const a =
+    'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;';
+  const b =
+    'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------';
+  const p = new RegExp(a.split('').join('|'), 'g');
+
+  return string
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(p, (c) => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+    .replace(/--+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, ''); // Trim - from end of text
+}
+
 export const RecipeCreatorPlugin = {
   __type: 'content-creator',
   name: 'New Recipe',
@@ -17,10 +38,27 @@ export const RecipeCreatorPlugin = {
       description: 'Recipe Instructions',
     },
   ],
-  async onSubmit(values, cms) {
+  async onSubmit(values: any, cms: CMS) {
+    // cms.api.github;
+    console.log(cms);
+    const fileRelativePath = `content/${slugify(values.title)}.json`;
     // Call functions that create the new blog post. For example:
     // cms.apis.someBackend.createPost(values);
     console.log({ values });
-    console.log(cms.apis);
+    cms.api.github
+      .upload(fileRelativePath, JSON.stringify(values), 'Update from TinaCMS')
+      .then((response) => {
+        console.log({ response });
+        // setCachedFormData(fileRelativePath, {
+        //   sha: response.content.sha,
+        // });
+        // if (this.afterCreate) {
+        //   this.afterCreate(response);
+        // }
+      })
+      .catch((e) => {
+        console.error(e);
+        // return { [FORM_ERROR]: e };
+      });
   },
 };
